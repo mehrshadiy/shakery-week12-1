75d399af80274134394b200e352ea4bdc1987044
mehrshadiy <mehrshadshakery@yahoo.com>
2023-10-02 16:09:23 +0330

    folding problems and animation fixed

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ for (let accordion of accordions) {
     function closeAll(){
         for (let accordionItem of accordionItems) {
             accordionItem.querySelector(".accordion-item__content").classList.remove("fold")
+            let accordionItemContent = accordionItem.querySelector(".accordion-item__content")
+            accordionItemContent.style.height="0px"
         }
     }
     for (let accordionItem of accordionItems) {
         let accordionItemBtn = accordionItem.querySelector(".accordion-item__btn")
-        let accordionItemContent = accordionItem.querySelector(".accordion-item__content")
-
         accordionItemBtn.onclick = (e)=>{
             if (e.target.value==="folded"){
                 e.target.nextElementSibling.classList.remove("fold")
                 e.target.value = ""
+                e.target.nextElementSibling.style.height = "0px"
             }else {
                 closeAll()
                 e.target.nextElementSibling.classList.add("fold")
                 e.target.value = "folded"
+                e.target.nextElementSibling.style.height = e.target.nextElementSibling.scrollHeight+"px"
             }
         }
     }
